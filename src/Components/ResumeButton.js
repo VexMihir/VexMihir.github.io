1376a1bbd5304b77da1cf174edb0db20a026631f
VexMihir <68463796+VexMihir@users.noreply.github.com>
2023-09-22 16:01:56 -0700

    resume update

--- src/Components/ResumeButton.js
+++ src/Components/ResumeButton.js
@@ -5,7 +5,7 @@ function ResumeButton() {
   return (
     <div class="btn">
       <div class="btn-container">
-        <a href ="https://drive.google.com/file/d/1d11KkR2mPYgi8v0HIy90X6Hj2kafvHJP/view?usp=sharing" target="_blank"><button class="resume">
+        <a href ="https://drive.google.com/file/d/1tA5pcJlMqdsPhlVi039jIDy0BTIXt1mM/view?usp=sharing" target="_blank"><button class="resume">
           <span class="text">Resume</span>
           <div class="icon-container">
             <div class="icon icon--left">
